d9f75ef617f84fc366556effcec68b2d5ad4adeb
Federico Fernandez Lafi <ffernandezlafi@gmail.com>
2023-04-20 20:39:40 -0300

    Agregando seccion about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,59 @@
-export const About = () => {
-    return (
-        <section id='about'>
+import "./About.css";
+import ImagenAbout from "../../assets/About/foto-about.jpg";
 
-        </section>
-    )
-}
+export const About = () => {
+  return (
+    <section id="about">
+      <div className="container">
+        <div className="contenedor-title"></div>
+        <div className="contendedor-p">
+          <h3 className="h3"> Hola! </h3>
+          <h3 className="h3 h3-chero"> Soy Cami</h3>
+          <div className="p">
+            <p>
+              Soy Diseñadora Gráfica, Industrial, Community Manager, e hice
+              diversos cursos: Marketing de contenidos, Gestión del Tiempo,
+              Cursos destinados a Emprendedores. Fotografia con celular y para
+              redes sociales, Marketing para Instagram Siempre estoy aprendiendo
+              y encontrando nuevos cursos o talleres de los cuales nutrirme. así
+              como leo diario muchos libros de crecimiento personal,
+              productividad, marketing, entre otros.
+            </p>
+          </div>
+          <div className="p">
+            <p>
+              Siempre estoy aprendiendo y encontrando nuevos cursos o talleres
+              de los cuales nutrirme. así como leo diario muchos libros de
+              crecimiento personal, productividad, marketing, entre otros.
+            </p>
+          </div>
+          <div className="p">
+            <p>
+              Anima es un canal de diseño gráfico y marketing que ha mutado en
+              diversos rubros del diseño a lo largo de sus 11 años con el finde
+              ayudar a emprendedores a llevar adelante sus emprendimientos y
+              encontrarse acompañados en un camino que normalmente es muy
+              solitario.
+            </p>
+          </div>
+          <div className="p">
+            <p>
+              Esta ayuda parte de la idea de poder plasmar gráficamente su magia
+              para que los demás lo conozcan.
+            </p>
+          </div>
+          <div className="p">
+            <p>
+              Contamos con experiencia para ayudar en el camino del emprendedor,
+              el cual si bien es bastante cuesta arriba, también tiene descansos
+              donde se disfruta muchísimo del paisaje y de lo logrado
+            </p>
+          </div>
+        </div>
+        <div className="contenedor-img">
+          <img className="imagen" src={ImagenAbout} alt="foto-acerca-de" />
+        </div>
+      </div>
+    </section>
+  );
+};
