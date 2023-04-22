ca014f04e7617cf1f4cae5cd724357786916a58e
gerfabris <ggerfabris@gmail.com>
2023-04-22 12:18:50 -0300

    Sidebar

--- src/components/AppRoutes/AppRoutes.jsx
+++ src/components/AppRoutes/AppRoutes.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
-import { Navbar } from "../Navbar/Navbar";
 import { WhatsApp } from "../WhatsApp/WhatssApp";
 import { Footer } from "../Footer/Footer";
 import { PublicRoutes } from "./PublicRoutes";
 import { About } from "../About/About";
+import { Sidebar } from "../Sidebar/Sidebar";
 
 export const AppRoutes = () => {
   return (
     <BrowserRouter>
-      <Navbar />
+        <Sidebar/>
       <PublicRoutes />
       <WhatsApp />
       <About />
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { Links } from "../../Data/Links";
+import { ItemSidebar } from "../ItemSidebar/ItemSidebar";
+import './Sidebar.css'
+
+export const Sidebar = () => {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <div className={ open ? "sidebarOpen" : "sidebar"}>
+      <svg
+        className="hamburger"
+        onClick={() => setOpen (!open)}
+        viewBox="0 0 18 12"
+      >
+        <path
+          d="M0 12H18V10H0V12ZM0 7H18V5H0V7ZM0 0V2H18V0H0Z"
+          fill="#8F8F8F"
+        />
+      </svg>
+      <div className="linksContainer">
+        {Links &&
+          Links.map (({to, text, svg}) => (
+            <ItemSidebar to={to} text={text} svg={svg} open={open} />
+          ))}
+      </div>
+    </div>
+  )
+}
