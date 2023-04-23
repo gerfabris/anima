cdcaf318213cd1b3b9b5b9175397f8248fecfab1
Federico Fernandez Lafi <ffernandezlafi@gmail.com>
2023-04-23 20:54:22 -0300

    agregando footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,47 @@
-export const Footer = () => {
-    return (
-        <footer id='footer'>
+import "./Footer.css";
+import { NavLink } from "react-router-dom";
 
-        </footer>
-    )
-}
+export const Footer = () => {
+  return (
+    <footer id="footer">
+      <div className="footer-container">
+        <div className="section-container">
+          <h4> SECCIONES </h4>
+          <NavLink>
+            <p> Inicio </p>
+          </NavLink>
+          <NavLink>
+            <p> Acerca de </p>
+          </NavLink>
+          <NavLink>
+            <p> Servicios </p>
+          </NavLink>
+          <NavLink>
+            <p> Portfolio </p>
+          </NavLink>
+          <NavLink>
+            <p> Contacto </p>
+          </NavLink>
+        </div>
+        <div className="data-container">
+          <h4> CONTACTO </h4>
+          <p> Direccion </p>
+          <p> Mail </p>
+          <p> Telefono</p>
+        </div>
+        <div className="socialmedia-container">
+          <h4> REDES </h4>
+          <NavLink>
+            <p> Instagram </p>
+          </NavLink>
+          <NavLink>
+            <p> Facebook </p>
+          </NavLink>
+          <NavLink>
+            <p> Whatsapp </p>
+          </NavLink>
+        </div>
+      </div>
+    </footer>
+  );
+};
