826f2970e4099e0fb4e1d0be754a9018192e57f4
Federico Fernandez Lafi <ffernandezlafi@gmail.com>
2023-05-23 21:00:27 -0300

    agregando fotos y cambios en data services

--- src/Data/DataServices.jsx
+++ src/Data/DataServices.jsx
@@ -4,41 +4,41 @@ export const services = [
     title: "Diseño de logotipo y Brandboard",
     description:
       "Creamos un logotipo que represente tu marca y refleje tus valores y guía de estilo para tu marca que incluye paleta de colores, tipografía, estilos de imagen y más..",
-    image: "/src/assets/logo/logo2 animanuevo@4x-8-blue-.png",
+    image: "/src/assets/Servicios/logos.png",
   },
   {
     id: 2,
     title: "Asesoría personalizada de Instagram",
     description:
       "Te ayudamos a optimizar tu perfil de Instagram, aumentar tu alcance y mejorar tu presencia en esta red social.",
-    image: "/src/assets/logo/logo2 animanuevo@4x-8-blue-.png",
+    image: "/src/assets/Servicios/asesoria.png",
   },
   {
     id: 3,
     title: "Diseño gráfico de redes para Instagram",
     description:
       "Creamos publicaciones para Instagram que atraigan a tu público objetivo y refuercen tu marca.",
-    image: "/src/assets/logo/logo2 animanuevo@4x-8-blue-.png",
+    image: "/src/assets/Servicios/diseno.png",
   },
   {
     id: 4,
     title: "Calendario de contenidos de Instagram",
     description:
       "Creamos un calendario de publicaciones para Instagram que te permitirá planificar tu contenido y asegurar que siempre estás comunicando de forma coherente.",
-    image: "/src/assets/logo/logo2 animanuevo@4x-8-blue-.png",
+    image: "/src/assets/Servicios/calendario.png",
   },
   {
     id: 5,
     title: "Pack de manejo de redes para Instagram",
     description:
       "Nos encargamos de la gestión completa de tus redes sociales, creando y publicando contenido, interactuando con tu audiencia y midiendo los resultados.",
-    image: "/src/assets/logo/logo2 animanuevo@4x-8-blue-.png",
+    image: "/src/assets/Servicios/social-media.png",
   },
   {
     id: 6,
     title: "Servicios Web",
     description:
       "En colaboración con Impulsar Dev Solutions, ofrecemos servicios de desarrollo web a medida para adaptarnos a las necesidades de cada cliente",
-    image: "/src/assets/logo/logo2 animanuevo@4x-8-blue-.png",
+    image: "/src/assets/Servicios/internet (1).png",
   },
 ];
