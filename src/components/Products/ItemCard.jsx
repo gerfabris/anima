624667d9c72204c14366db97d330be2fa7991493
gerfabris <ggerfabris@gmail.com>
2023-05-25 15:13:16 -0300

    Stylers services card

--- src/components/Products/ItemCard.jsx
+++ src/components/Products/ItemCard.jsx
@@ -17,18 +17,18 @@ export const ItemCard = ( {service , index , onClick } ) =>{
                 onClick={ handleClick }
             >
                 <div className='card__front'>
-            {/*         <div className='card__image-wrapper'> 
+                    <h2 className='card__content-title' > {service.title} </h2>
+                    <div className='card__image-wrapper'> 
                         <img src= {service.image} alt='' className='card__image' />
                     </div>
                     <div className='card__content'>
-                        <h2 className='card__content-title' > {service.title} </h2>
                         <span className='card__content-arrow'></span>
-                    </div> */}
+                    </div>
                 </div>
                 <div className="card__back">
-        {/*             <div className='card__content'>
+                    <div className='card__content'>
                         <p className="card__content-description">{service.description}</p>
-                    </div> */}
+                    </div>
                 </div>
             </article>
     )
