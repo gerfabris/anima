5b2fbaff4a3c3dfd831f593bea54312d739a1a0e
Federico Fernandez Lafi <ffernandezlafi@gmail.com>
2023-05-27 19:19:24 -0300

    agregando testimonios

---
+++ src/Data/DataTestimonio.jsx
@@ -0,0 +1,72 @@
+export const testimonios = [
+  {
+    id: 1,
+    cliente: "Nicole - Muelería Serendipia",
+    mensaje:
+      "Muy linda experiencia, Cami una genia y muy amable. El material que nos entregó es super útil y claro. Sin dudas nos ayudo un montón a mejorar nuestras redes",
+    servicioRealizado: "Asesoría de Instagram",
+  },
+  {
+    id: 2,
+    cliente: "Eugenia - Endrógena",
+    mensaje:
+      "Creamos un logotipo que represente tu marca y refleje tus valores y guía de estilo para tu marca que incluye paleta de colores, tipografía, estilos de imagen y más..",
+    servicioRealizado: "Creación de Logotipo.",
+  },
+  {
+    id: 3,
+    cliente: "Andrea - Psicologa",
+    mensaje:
+      "Estoy muy satisfecha con el servicio de logo, es la primera vez que me aventuro a un proceso así, y me gustó mucho desde el primer momento, me sentí acompañada. Los cuestionarios son una gran guía, donde una puede plasmar las ideas, y las preguntas ayudan también, porque a veces, una tiene una vaga idea, pero las preguntas te hacen reflexionar bastante y ayuda. También la amabilidad y calidez desde quien llevó a cabo Anima, ayuda en este proceso de entendimiento, de familiarizarse con el procceso, y la guía de cómo seguirá. Me quedo muy conforme con el servicio. sin dudas lo voy a recomendar a mis conocidos, ya que fue una expriencia muy linda",
+    servicioRealizado: "Creación de Logotipo.",
+  },
+  {
+    id: 4,
+    cliente: "Sofía - C. Treinta y Tres",
+    mensaje:
+      "Cami, la verdad estamos felices por el logo, no podemos creer lo lindo que quedó y que hayas captado a la perfección nuestras ideas. Durante todo el proceso nos evacuaste todas las dudas. También quería agradecerte por la amabilidad y dulzura que transimitís en los mensajes, eso está demás porque nos hizo sentir que podíamos confiar en tí y no nos equivocamos",
+    servicioRealizado: "Creación de Logotipo.",
+  },
+  {
+    id: 5,
+    cliente: "Cami - Amigurumis",
+    mensaje:
+      "Me gustó todo en realidad, súper interesante. Voy a comenzar a aplicar las listas de tareas, me parece importante para organizarme mejor y no olvidar nada, y la claridad con la que expresaste todo, súper entendible. Y..me resonó el método Pomodoro, creo que lo voy a comenzar a utilizar. Agradecerte por tu tiempo y por brindarnos este taller. Muchas gracias!",
+    servicioRealizado: `Participación en taller práctico "¿Como lo hago?"`,
+  },
+  {
+    id: 6,
+    cliente: "Ana Claudia - La Gota",
+    mensaje:
+      "Cami, entendió perfectamente desde el día uno, el propósito de nuestra marca y su razón de ser.     Así logró crear nuestra imagen de marca, creando nuestro logo y todo el material de diseño gráfico que necesitamos cada vez que se lo solicitamos. se ajuste a lo que deseamos transmitir e Empatiza con sus clientes y siempre busca la forma de que sus diseños se ajusten a lo que deseamos transmitir",
+    servicioRealizado: "Creación de Logotipo.",
+  },
+  {
+    id: 7,
+    cliente: "Natalia - Joyería y accesorios",
+    mensaje:
+      "Lo que mas me gusto fue el video extra de las reglas, siempre soy de postergar esas pequeñas cosas para después hacerlas todas juntas y me termina llevando mas tiempo que si la hubiese realizado en pequeños ratitos. Poder aprender como organizar mis tareas para poder aprovechar al máximo el tiempo. Estuvo muy completo y muy bien explicado. No mejoraría nada.",
+    servicioRealizado: `Participación en taller práctico "¿Como lo hago?"`,
+  },
+  {
+    id: 8,
+    cliente: "Sofía - Diseño de Joyas",
+    mensaje:
+      "Me gustaron los métodos para mejorar mi organización semanal.La variedad de ideas para una mejor organización. El planteo de poner un horario, por ejemplo, a responder mensajes ya que estoy todo el día respondiendo consultas y tomando pedidos. Darle prioridad a los findes libres lo cual hace poco tiempo logre. Me resulto de mucha ayuda e informativo.Super completo. Muchas gracias por el espacio y la oportunidad!",
+    servicioRealizado: `Participación en taller práctico "¿Como lo hago?"`,
+  },
+  {
+    id: 9,
+    cliente: "Sabrina - Papelería y Diseño Gráfico",
+    mensaje:
+      "Me gusto mucho la parte de como planificar el contenido, el mes completo. El saber que contenido subir y en que momento. Y lo voy a poner a prueba. Me pareció super completo! Me encantó y me hizo pensar en las cosas que necesito para llevar mi emprendimiento un pasito más allá. Así que quería agradecerte por todo, por el taller, por los descargables, y sobre todo por tu tiempo, por tomarte el tiempo necesario para hacer este taller y que sea gratis es un montón! Así que muchas gracias",
+    servicioRealizado: `Participación en taller práctico "¿Como lo hago?"`,
+  },
+  {
+    id: 10,
+    cliente: "Sofía - Terra Depilación",
+    mensaje:
+      "Me encanta tu estilo de trabajo, sos atenta y detallista. También considero que suma tu trato, siempre amable, dispuesta y con respuesta rápida y sobre todo paciente. Muchas gracias Cami",
+    servicioRealizado: "Gestión de Redes",
+  },
+];
