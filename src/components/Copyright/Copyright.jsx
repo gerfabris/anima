aca5269190b8f18994198a72802b2db0802bfc32
Federico Fernandez Lafi <ffernandezlafi@gmail.com>
2023-05-29 18:40:01 -0300

    acomodando detalles

--- src/components/Copyright/Copyright.jsx
+++ src/components/Copyright/Copyright.jsx
@@ -3,7 +3,7 @@ import LogoCopyright from "../../assets/Copyright/Composicion1-2.png";
 
 export const Copyright = () => {
   return (
-    <div className="container">
+    <div className="copy__container">
       <div className="item__container">
         <h5> Desarrollo web por </h5>
         <img src={LogoCopyright} alt="Impulsar Dev Solutions" />
