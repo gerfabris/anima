961b5c3b2a181e229bca0d17cedf1a501fe41681
gerfabris <ggerfabris@gmail.com>
2023-06-07 18:35:59 -0300

    image home y others

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,15 @@
 import { Sidebar } from "../Sidebar/Sidebar";
 import Ilustracion from "../../assets/Home/Cami.png";
+import IlustracionMobile from "../../assets/Home/Cami-mobile.png";
 import { TestimoniosSwiper } from "../TestimoniosSwiper/TestimoniosSwiper";
 import { dataTestimonios } from "../../Data/DataTestimonio";
 
 import "./Home.css";
+import { useImageSwitch } from "./useImageSwitch ";
 /* -- -- */
 export const Home = () => {
   const testimonios = dataTestimonios;
+  const imageUrl = useImageSwitch(Ilustracion, IlustracionMobile);
 
   return (
     <>
@@ -38,7 +41,7 @@ export const Home = () => {
             </h3>
           </div>
           <div className="homeDiv container__home-ilustration">
-            <img src={Ilustracion} alt="anima" className="ilustration__image" />
+            <img src={imageUrl} alt="anima" className="ilustration__image" />
           </div>
         </article>
         <article
