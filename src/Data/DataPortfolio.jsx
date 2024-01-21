5e7078aefae29dcb821e1ff3313de0aa24cb923a
gerfabris <ggerfabris@gmail.com>
2024-01-21 19:27:26 -0300

    Add images

--- src/Data/DataPortfolio.jsx
+++ src/Data/DataPortfolio.jsx
@@ -1,10 +1,50 @@
+/* import img logos */
+import ImgPortfolioLogos1 from "../assets/img/DataPortfolio/logos/1.webp";
+import ImgPortfolioLogos2 from "../assets/img/DataPortfolio/logos/2.webp";
+import ImgPortfolioLogos3 from "../assets/img/DataPortfolio/logos/3.webp";
+import ImgPortfolioLogos4 from "../assets/img/DataPortfolio/logos/4.webp";
+import ImgPortfolioLogos5 from "../assets/img/DataPortfolio/logos/5.webp";
+import ImgPortfolioLogos6 from "../assets/img/DataPortfolio/logos/6.webp";
+import ImgPortfolioLogos7 from "../assets/img/DataPortfolio/logos/7.webp";
+import ImgPortfolioLogos8 from "../assets/img/DataPortfolio/logos/8.webp";
+import ImgPortfolioLogos9 from "../assets/img/DataPortfolio/logos/9.webp";
+import ImgPortfolioLogos10 from "../assets/img/DataPortfolio/logos/10.webp";
+import ImgPortfolioLogos11 from "../assets/img/DataPortfolio/logos/11.webp";
+import ImgPortfolioLogos12 from "../assets/img/DataPortfolio/logos/12.webp";
+import ImgPortfolioLogos13 from "../assets/img/DataPortfolio/logos/13.webp";
+import ImgPortfolioLogos14 from "../assets/img/DataPortfolio/logos/14.webp";
+import ImgPortfolioLogos15 from "../assets/img/DataPortfolio/logos/15.webp";
+import ImgPortfolioLogos16 from "../assets/img/DataPortfolio/logos/16.webp";
+/* -- img otros -- */
+import ImgPortfolioOtros19 from "../assets/img/DataPortfolio/otros/19.webp";
+import ImgPortfolioOtros20 from "../assets/img/DataPortfolio/otros/20.webp";
+import ImgPortfolioOtros21 from "../assets/img/DataPortfolio/otros/21.webp";
+import ImgPortfolioOtros22 from "../assets/img/DataPortfolio/otros/22.webp";
+import ImgPortfolioOtros23 from "../assets/img/DataPortfolio/otros/23.webp";
+import ImgPortfolioOtros24 from "../assets/img/DataPortfolio/otros/24.webp";
+import ImgPortfolioOtros25 from "../assets/img/DataPortfolio/otros/25.webp";
+import ImgPortfolioOtros26 from "../assets/img/DataPortfolio/otros/26.webp";
+import ImgPortfolioOtros27 from "../assets/img/DataPortfolio/otros/27.webp";
+/* import img redes */
+import ImgPortfolioRedes28 from "../assets/img/DataPortfolio/redes/28.webp";
+import ImgPortfolioRedes29 from "../assets/img/DataPortfolio/redes/29.webp";
+import ImgPortfolioRedes30 from "../assets/img/DataPortfolio/redes/30.webp";
+import ImgPortfolioRedes31 from "../assets/img/DataPortfolio/redes/31.webp";
+import ImgPortfolioRedes32 from "../assets/img/DataPortfolio/redes/32.webp";
+import ImgPortfolioRedes33 from "../assets/img/DataPortfolio/redes/33.webp";
+import ImgPortfolioRedes34 from "../assets/img/DataPortfolio/redes/34.webp";
+import ImgPortfolioRedes35 from "../assets/img/DataPortfolio/redes/35.webp";
+import ImgPortfolioRedes36 from "../assets/img/DataPortfolio/redes/35.webp";
+import ImgPortfolioRedes37 from "../assets/img/DataPortfolio/redes/37.webp";
+/* -- Component  -- */
 export const dataPorfolio = [
   {
     id: 1,
     title: "Plana",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1tvHUvkwOYTZWldYNMDgOzc2jt_ZPkPro",
+      ImgPortfolioLogos1,
+      /* "https://drive.google.com/uc?export=view&id=1tvHUvkwOYTZWldYNMDgOzc2jt_ZPkPro", */
     category: "logos",
   },
   {
@@ -12,7 +52,8 @@ export const dataPorfolio = [
     title: "Karla Yaben",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=12sQ6odK1wT3Ibfy1r5texcVsvJb1p3J1",
+      ImgPortfolioLogos2,
+      /* "https://drive.google.com/uc?export=view&id=12sQ6odK1wT3Ibfy1r5texcVsvJb1p3J1", */
     category: "logos",
   },
   {
@@ -20,7 +61,8 @@ export const dataPorfolio = [
     title: "Licenciada Andrea Seoane",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1kfa-M17JUicYEhVQN_fu7BAwjriuYIe2",
+      ImgPortfolioLogos3,
+      /* "https://drive.google.com/uc?export=view&id=1kfa-M17JUicYEhVQN_fu7BAwjriuYIe2", */
     category: "logos",
   },
   {
@@ -28,7 +70,8 @@ export const dataPorfolio = [
     title: "Boutique Gaman",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1OivORfmCWfewBIzfXxgDSgb-GYG5epF-",
+      ImgPortfolioLogos4,
+      /* "https://drive.google.com/uc?export=view&id=1OivORfmCWfewBIzfXxgDSgb-GYG5epF-", */
     category: "logos",
   },
   {
@@ -36,7 +79,8 @@ export const dataPorfolio = [
     title: "Cabaña Odín",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1hj1S8x3JYWNa41tnXo5_IXC5rbN8MK2C",
+      ImgPortfolioLogos5,
+      /* "https://drive.google.com/uc?export=view&id=1hj1S8x3JYWNa41tnXo5_IXC5rbN8MK2C", */
     category: "logos",
   },
   {
@@ -44,7 +88,8 @@ export const dataPorfolio = [
     title: "Endógena Autoras",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1kwjFMbxMGM2iJN4jKp8czNiIUJsioIzh",
+      ImgPortfolioLogos6,
+      /* "https://drive.google.com/uc?export=view&id=1kwjFMbxMGM2iJN4jKp8czNiIUJsioIzh", */
     category: "logos",
   },
   {
@@ -52,7 +97,8 @@ export const dataPorfolio = [
     title: "Out of the Box",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1brhmEzgoVRxsqM8B-Q9fjJuExi5iDoEl",
+      ImgPortfolioLogos7,
+      /* "https://drive.google.com/uc?export=view&id=1brhmEzgoVRxsqM8B-Q9fjJuExi5iDoEl", */
     category: "logos",
   },
   {
@@ -60,7 +106,8 @@ export const dataPorfolio = [
     title: "Mi Julián Artesanías",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1yVY1_kN2nOlM5gpBhqUKpRBAfq3t4wEa",
+      ImgPortfolioLogos8,
+      /* "https://drive.google.com/uc?export=view&id=1yVY1_kN2nOlM5gpBhqUKpRBAfq3t4wEa", */
     category: "logos",
   },
   {
@@ -68,7 +115,8 @@ export const dataPorfolio = [
     title: "La Gota",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1tGaQFW1jJxitX26GJwaHaYoFM1lUQ58q",
+      ImgPortfolioLogos9,
+      /* "https://drive.google.com/uc?export=view&id=1tGaQFW1jJxitX26GJwaHaYoFM1lUQ58q", */
     category: "logos",
   },
   {
@@ -76,7 +124,8 @@ export const dataPorfolio = [
     title: "Cabaña El Martillo",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1bAnV9CbBAD3t56BkwDuu7gPRBizoHz10",
+      ImgPortfolioLogos10,
+      /* "https://drive.google.com/uc?export=view&id=1bAnV9CbBAD3t56BkwDuu7gPRBizoHz10", */
     category: "logos",
   },
   {
@@ -84,7 +133,8 @@ export const dataPorfolio = [
     title: "Cabaña Treinta y Tres",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1spSCG3QDJqbWQbEWgIE0Fe68SJ8UrVx-",
+      ImgPortfolioLogos11,
+      /* "https://drive.google.com/uc?export=view&id=1spSCG3QDJqbWQbEWgIE0Fe68SJ8UrVx-", */
     category: "logos",
   },
   {
@@ -92,7 +142,8 @@ export const dataPorfolio = [
     title: "NUDM",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1-1Yd4zWN3vIzlPgPgTIjsCUp1-_YGE2P",
+      ImgPortfolioLogos12,
+      /* "https://drive.google.com/uc?export=view&id=1-1Yd4zWN3vIzlPgPgTIjsCUp1-_YGE2P", */
     category: "logos",
   },
   {
@@ -100,7 +151,8 @@ export const dataPorfolio = [
     title: "Dejá tu huella",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1UfZm4TTIGOn5tO6du3tSmRQaCJwwuKZu",
+      ImgPortfolioLogos13,
+      /* "https://drive.google.com/uc?export=view&id=1UfZm4TTIGOn5tO6du3tSmRQaCJwwuKZu", */
     category: "logos",
   },
   {
@@ -108,7 +160,8 @@ export const dataPorfolio = [
     title: "Crila Designs",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1nbj6fG9fStoFZ0QxuN8rEqM0Y01r2JwR",
+      ImgPortfolioLogos14,
+      /* "https://drive.google.com/uc?export=view&id=1nbj6fG9fStoFZ0QxuN8rEqM0Y01r2JwR", */
     category: "logos",
   },
   {
@@ -116,7 +169,8 @@ export const dataPorfolio = [
     title: "Shineuy",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1-hCQWlyANc5UHduIyGpwrT-1m3F8H00T",
+      ImgPortfolioLogos15,
+      /* "https://drive.google.com/uc?export=view&id=1-hCQWlyANc5UHduIyGpwrT-1m3F8H00T", */
     category: "logos",
   },
   {
@@ -124,31 +178,35 @@ export const dataPorfolio = [
     title: "Nutriendo",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1alM5W2vIgNgoPbh2rOK8BkiZdx7NSb8w",
+      ImgPortfolioLogos16,
+      /* "https://drive.google.com/uc?export=view&id=1alM5W2vIgNgoPbh2rOK8BkiZdx7NSb8w", */
     category: "logos",
   },
-  {
+/*   {
     id: 17,
-    title: "",
+    title: "MJ",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1atLhEyimIXrGjUH6hlKrNFylnuylU7vq",
-    category: "otros",
-  },
-  {
+      ImgPortfolioLogos17,
+      /* "https://drive.google.com/uc?export=view&id=1rPzmXoDwnX0w1TJeJ3noKrQ8dHe4uTtO",
+    category: "logos",
+  }, */
+/*   {
     id: 18,
-    title: "",
+    title: "Lic. Andrea Soane",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1LWPvvo5oAZsxX4rlazXi4LdUTWZ5xLDG",
-    category: "otros",
-  },
+      ImgPortfolioLogos18,
+      /* "https://drive.google.com/uc?export=view&id=1BB-cJHNFbNEqKLSH4g_7ho1_TMDH6sB7",
+    category: "logos",
+  }, */
   {
     id: 19,
     title: "",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1rwn5tS1--wgPIFhC_TM9_feiE7d4k74M",
+      ImgPortfolioOtros19,
+      /* "https://drive.google.com/uc?export=view&id=1atLhEyimIXrGjUH6hlKrNFylnuylU7vq", */
     category: "otros",
   },
   {
@@ -156,7 +214,8 @@ export const dataPorfolio = [
     title: "",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1c_tQ_mxbnyd6ZilStGAHT5Lg_X0kDwTy",
+      ImgPortfolioOtros20,
+      /* "https://drive.google.com/uc?export=view&id=1LWPvvo5oAZsxX4rlazXi4LdUTWZ5xLDG", */
     category: "otros",
   },
   {
@@ -164,63 +223,89 @@ export const dataPorfolio = [
     title: "",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1LPp7aZuYtvzGe8flO7rfvxIwKAQ7he6G",
-    category: "redes",
+      ImgPortfolioOtros21,
+      /* "https://drive.google.com/uc?export=view&id=1rwn5tS1--wgPIFhC_TM9_feiE7d4k74M", */
+    category: "otros",
   },
   {
     id: 22,
     title: "",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1EIuiyMEo-QCXcGe5IVvhe4XB0BVztnKf",
-    category: "redes",
+      ImgPortfolioOtros22,
+      /* "https://drive.google.com/uc?export=view&id=1c_tQ_mxbnyd6ZilStGAHT5Lg_X0kDwTy", */
+    category: "otros",
   },
   {
     id: 23,
     title: "",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1PyyLxlStE0_WJJ6J4DcvY9FE5zGjm-8z",
-    category: "redes",
+      ImgPortfolioOtros23,
+      /* "https://drive.google.com/uc?export=view&id=1Ra1HvCJKc_KSfkhuIvtZoAvOCIfrebuE", */
+    category: "otros",
   },
   {
     id: 24,
     title: "",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1PolfLbkoFps0bHdE1x7_ltVW8HLyZcW1",
-    category: "redes",
+        ImgPortfolioOtros24,
+      /* "https://drive.google.com/uc?export=view&id=1MDn7fOhQ-w7ULEMI8ERlMNssWCXaAT2z", */
+    category: "otros",
   },
   {
     id: 25,
     title: "",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1AekBD5c93fxXRq2WIIrNscx6KtVYQZlE",
-    category: "redes",
+      ImgPortfolioOtros25,
+      /* "https://drive.google.com/uc?export=view&id=1YF5JKLHedvXyGBbNy0Ri2gkYH4FXAXrY", */
+    category: "otros",
   },
   {
     id: 26,
-    title: "MJ",
+    title: "",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1rPzmXoDwnX0w1TJeJ3noKrQ8dHe4uTtO",
-    category: "logos",
+      ImgPortfolioOtros26,
+      /* "https://drive.google.com/uc?export=view&id=1GXiZ3yobOyipeOUG3n73EP-6P1FNgPFB", */
+    category: "otros",
   },
   {
     id: 27,
-    title: "Lic. Andrea Soane",
+    title: "",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1BB-cJHNFbNEqKLSH4g_7ho1_TMDH6sB7",
-    category: "logos",
+      ImgPortfolioOtros27,
+      /* "https://drive.google.com/uc?export=view&id=10Rz_DWwkLzLhBqPpd7rOGiieShHtiCCO", */
+    category: "otros",
+  },
+  {
+    id: 28,
+    title: "",
+    description: "",
+    image:
+      ImgPortfolioRedes28,
+      /* "https://drive.google.com/uc?export=view&id=1LPp7aZuYtvzGe8flO7rfvxIwKAQ7he6G", */
+    category: "redes",
+  },
+  {
+    id: 29,
+    title: "",
+    description: "",
+    image:
+      ImgPortfolioRedes29,
+      /* "https://drive.google.com/uc?export=view&id=1hPk53Ex4K4uJedPI_ZX1DSePHtaEm0dE", */
+    category: "redes",
   },
   {
     id: 30,
     title: "",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1VXbvy8DuPbNptrPcJaErNWRldppYxVqs",
+      ImgPortfolioRedes30,
+      /* "https://drive.google.com/uc?export=view&id=1EIuiyMEo-QCXcGe5IVvhe4XB0BVztnKf", */
     category: "redes",
   },
   {
@@ -228,7 +313,8 @@ export const dataPorfolio = [
     title: "",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=16ZAeOD_UYg4WCvbJsQpaF--xQohLMj7M",
+      ImgPortfolioRedes31,
+      /* "https://drive.google.com/uc?export=view&id=1PyyLxlStE0_WJJ6J4DcvY9FE5zGjm-8z", */
     category: "redes",
   },
   {
@@ -236,7 +322,8 @@ export const dataPorfolio = [
     title: "",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=13ebeD346J7riL3cFHZrttYoJDwx2oHEq",
+      ImgPortfolioRedes32,
+      /* "https://drive.google.com/uc?export=view&id=1PolfLbkoFps0bHdE1x7_ltVW8HLyZcW1", */
     category: "redes",
   },
   {
@@ -244,55 +331,45 @@ export const dataPorfolio = [
     title: "",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1Ra1HvCJKc_KSfkhuIvtZoAvOCIfrebuE",
-    category: "otros",
+      ImgPortfolioRedes33,
+      /* "https://drive.google.com/uc?export=view&id=1AekBD5c93fxXRq2WIIrNscx6KtVYQZlE", */
+    category: "redes",
   },
   {
     id: 34,
     title: "",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1MDn7fOhQ-w7ULEMI8ERlMNssWCXaAT2z",
-    category: "otros",
+      ImgPortfolioRedes34,
+      /* "https://drive.google.com/uc?export=view&id=1VXbvy8DuPbNptrPcJaErNWRldppYxVqs", */
+    category: "redes",
   },
   {
     id: 35,
     title: "",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1YF5JKLHedvXyGBbNy0Ri2gkYH4FXAXrY",
-    category: "otros",
-  },
-  {
-    id: 39,
-    title: "",
-    description: "",
-    image:
-      "https://drive.google.com/uc?export=view&id=1eWSDA1J_VJopkVTNol4q9LiBzEfQXKDb",
+      ImgPortfolioRedes35,
+      /* "https://drive.google.com/uc?export=view&id=16ZAeOD_UYg4WCvbJsQpaF--xQohLMj7M", */
     category: "redes",
   },
   {
-    id: 40,
+    id: 36,
     title: "",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1hPk53Ex4K4uJedPI_ZX1DSePHtaEm0dE",
+      ImgPortfolioRedes36,
+      /* "https://drive.google.com/uc?export=view&id=13ebeD346J7riL3cFHZrttYoJDwx2oHEq", */
     category: "redes",
   },
+
   {
-    id: 41,
+    id: 37,
     title: "",
     description: "",
     image:
-      "https://drive.google.com/uc?export=view&id=1GXiZ3yobOyipeOUG3n73EP-6P1FNgPFB",
-    category: "otros",
-  },
-  {
-    id: 42,
-    title: "",
-    description: "",
-    image:
-      "https://drive.google.com/uc?export=view&id=10Rz_DWwkLzLhBqPpd7rOGiieShHtiCCO",
-    category: "otros",
+      ImgPortfolioRedes37,
+      /* "https://drive.google.com/uc?export=view&id=1eWSDA1J_VJopkVTNol4q9LiBzEfQXKDb", */
+    category: "redes",
   },
 ];
